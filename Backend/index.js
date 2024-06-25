4b721141305fac802b661043f72149f306e92cfe
Rushikesh Bhalkar <98863246+Rushi12bhalkar@users.noreply.github.com>
2024-06-25 08:27:32 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(cors(
    {
-        origin: ["https://login-and-signup-mern-frontendproject.vercel.app/"],
+        origin: ["https://login-and-signup-mern-frontendproject.vercel.app"],
         methods:["POST", "GET"],
         credentials: true     
    }
